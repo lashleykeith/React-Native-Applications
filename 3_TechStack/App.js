69625ac9d3a4d482ccc6bcd9f23862febec4a8d0
Keith Lashley <lashleykeith@gmail.com>
2018-12-28 04:09:29 +0900

    Add files via upload

---
+++ 3_TechStack/App.js
@@ -0,0 +1,11 @@
+import { AppRegistry } from 'react-native';
+import App from './src/app';
+
+
+
+export default App;
+
+AppRegistry.registerComponent('tech_stack', ()=> App);
+
+// https://github.com/react-community/create-react-native-app/issues/144
+//4:17
